1e74498f22bce8dff5347d65e4e8ba82ceb0903a
kumarpramodlohar <kumarpramod.asn@gmail.com>
2022-03-31 22:19:43 +0530

    recent

--- src/main/java/io/gateways/userservice/UserserviceApplication.java
+++ src/main/java/io/gateways/userservice/UserserviceApplication.java
@@ -1,6 +1,10 @@
 package io.gateways.userservice;
 
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.gateways.userservice.service.UserService;
 
 
 @SpringBootApplication
@@ -28,26 +34,23 @@ public class UserserviceApplication {
 
 	
 
-//	@Bean
-	//CommandLineRunner run(UserService userService) {
-	//	return args -> {
-		//	userService.saveUser(new User(null,"Administrator","admin","admin","15678910","Y","admin","123456",new ArrayList<>()));
-		//	userService.addRoleToUser("admin", "ROLE_ADMIN");
-//
-//			userService.saveUser(new User(null, "Pramod Lohar", "pramod", "pramod1234", new ArrayList<>()));
-//			userService.saveUser(new User(null, "Subhamay Mondal", "subhamay", "subhamay1234", new ArrayList<>()));
-//			userService.saveUser(new User(null, "Avishto Banerjee", "avishto", "avistho1234", new ArrayList<>()));
-//			userService.saveUser(new User(null, "ShibShankar Pradhan", "shibShankar", "shibShankar1234", new ArrayList<>()));
-//
-//			userService.addRoleToUser("pramod", "ROLE_USER");
-//			userService.addRoleToUser("shibShankar", "ROLE_MANAGER");
-//			userService.addRoleToUser("shibShankar", "ROLE_USER");
-//			userService.addRoleToUser("shibShankar", "ROLE_ADMIN");
-//			userService.addRoleToUser("avishto", "ROLE_ADMIN");
-//			userService.addRoleToUser("subhamay", "ROLE_USER");
-//
-	//	};
-	//}
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			try {
+			      File myObj = new File("cookie.txt");
+			      if (myObj.createNewFile()) {
+			        System.out.println("File created: " + myObj.getName());
+			      } else {
+			        System.out.println("File already exists.");
+			      }
+			    } catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			    }
+
+		};
+	}
 	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
